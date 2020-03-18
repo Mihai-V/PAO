2c39f719b0260cdab7149d8e698687d3546e6c71
Mihai Vasilescu <44552815+Mihai-V@users.noreply.github.com>
2020-03-18 12:40:44 +0200

    Add files via upload

---
+++ Laborator 2/Exercitiul 2/exercitiul2.java
@@ -0,0 +1,9 @@
+public class exercitiul2 {
+    public static void main(String[] args) {
+        Person p1 = new Person("Doe", "John", 24, "1123444", "student");
+        Person p2 = new Person("Roe", "Jane", 56, "2233444", "teacher");
+
+        System.out.println(p1.getSurname() + " " + p1.getName() + " " + p1.getAge() + " " + p1.getIdNo() + " " + p1.getType());
+        System.out.println(p2.getSurname() + " " + p2.getName() + " " + p2.getAge() + " " + p2.getIdNo() + " " + p2.getType());
+    }
+}
