a1ab4857206a35db8174bc96aa210a68e9a7fa69
Mihai Vasilescu <44552815+Mihai-V@users.noreply.github.com>
2020-05-04 01:54:44 +0300

    Add files via upload

---
+++ Proiect/Etapa 2/device/Device.java
@@ -0,0 +1,43 @@
+package device;
+
+import java.util.ArrayList;
+
+public class Device {
+    private ArrayList<Phone> phones;
+    private ArrayList<Tablet> tablets;
+    private ArrayList<Laptop> laptops;
+
+    public Device() {
+        this.phones = new ArrayList<Phone>();
+        this.tablets = new ArrayList<Tablet>();
+        this.laptops = new ArrayList<Laptop>();
+    }
+
+    public ArrayList<Phone> getPhones() {
+        return phones;
+    }
+
+    public ArrayList<Tablet> getTablets() {
+        return tablets;
+    }
+
+    public ArrayList<Laptop> getLaptops() {
+        return laptops;
+    }
+
+    protected void deviceID(){
+    }
+
+    public void addPhone(Phone phone){
+        phones.add(phone);
+    }
+
+    public void addTablet(Tablet tablet){
+        tablets.add(tablet);
+    }
+
+    public void addLaptop(Laptop laptop){
+        laptops.add(laptop);
+    }
+}
+
---
+++ Proiect/Etapa 2/device/Laptop.java
@@ -0,0 +1,51 @@
+package device;
+
+public class Laptop extends Device {
+    private String manufacturer;
+    private String model;
+    private int RAM;
+    private int storage;
+
+    public Laptop(String manufacturer, String model, int RAM, int storage){
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.RAM = RAM;
+        this.storage = storage;
+    }
+
+    @Override
+    public void deviceID() {
+        System.out.println("device.Laptop: " + this.getManufacturer() + " " + this.getModel());
+        System.out.println("RAM: " + this.RAM + "GB");
+        System.out.print("Storage: ");
+        if (this.storage < 1024)
+            System.out.println(this.storage + "GB");
+        else
+            System.out.println(this.storage/1024 + "TB");
+        System.out.println();
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getRAM() {
+        return RAM;
+    }
+
+    public int getStorage() {
+        return storage;
+    }
+
+    public void setRAM(int RAM) {
+        this.RAM = RAM;
+    }
+
+    public void setStorage(int storage) {
+        this.storage = storage;
+    }
+}
